2bb99b4a05360771183d7cf7bbf90e7be1c5fad5
WEREMSOFT <pablo.weremczuk@gmail.com>
2020-01-05 15:36:15 +0100

    removed nasty stuff

--- src/main.c
+++ src/main.c
@@ -10,57 +10,109 @@
 ********************************************************************************************/
 
 #include "raylib.h"
-#if defined(PLATFORM_WEB)
-    #include <emscripten/emscripten.h>
-#endif
 #include "flecs.h"
-#include "game.h"
-#include "cube.h"
-#include "height_map.h"
+#include <math.h>
 
-enum game_level {
-    LEVEL_CUBE,
-    LEVEL_HEIGHT_MAP,
-    LEVEL_COUNT
-};
+Camera3D camera;
 
-GameContext game_context;
-uint current_level;
-ecs_world_t** levels[LEVEL_COUNT];
+void cube_init_position(ecs_rows_t * rows) {
+    ECS_COLUMN(rows, Vector3, position, 1);
 
-void handleControllers(void){
-    if (IsKeyPressed(KEY_SPACE) || IsMouseButtonReleased(MOUSE_LEFT_BUTTON) || IsMouseButtonReleased(MOUSE_RIGHT_BUTTON))
-    {
-        current_level = (current_level + 1) % LEVEL_COUNT;
-        game_context.world = levels[current_level];
+    for(int i = 0; i < rows->count; i++) {
+        position[i].x = 0;
+        position[i].y = 0;
+        position[i].z = 0;
     }
 }
 
-void game_update(void) {
-    ecs_progress(game_context.world, 0);
-    handleControllers();
+
+
+void cube_render(ecs_rows_t *rows) {
+
+    ECS_COLUMN(rows, Vector3, position, 1);
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    BeginMode3D(camera);
+
+    for (int i = 0; i < rows->count; i ++) {
+        DrawCube(position[i], 2.0f, 2.0f, 2.0f, CLITERAL(Color){11, 110, 176, 255});
+        DrawCubeWires(position[i], 2.0f, 2.0f, 2.0f, MAROON);
+    }
+
+    DrawGrid(10, 1.0f);
+    EndMode3D();
+    DrawFPS(10, 10);
+    EndDrawing();
 }
+
+void cube_update_position(ecs_rows_t *rows) {
+    static float phase = 0;
+    phase += 1.0 * rows->delta_time;
+    ECS_COLUMN(rows, Vector3, position, 1);
+    for(int i = 0; i < rows->count; i++) {
+        camera.position.x = sin(phase) * 10;
+        camera.position.z = cos(phase) * 10;
+        camera.position.y = 10;
+
+    }
+}
+
+void cube_init_world(ecs_world_t** world, int argc, char** argv){
+    *world = ecs_init_w_args(argc, argv);
+    ECS_TAG(*world, Rendereable);
+    ECS_COMPONENT(*world, Vector3);
+
+    ECS_SYSTEM(*world, cube_render, EcsOnUpdate, Vector3, Rendereable);
+
+    ECS_SYSTEM(*world, cube_init_position, EcsOnAdd, Vector3);
+
+    ECS_SYSTEM(*world, cube_update_position, EcsOnUpdate, Vector3);
+
+    ECS_ENTITY(*world, cube, Vector3, Rendereable);
+    ecs_set_target_fps(*world, 120);
+}
+
+void init_camera() {
+    printf("Initializando\n");
+
+    InitWindow(800, 600, "raylib [core] example - 3d camera free");
+
+    // Define the camera to look into our 3d world
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.type = CAMERA_PERSPECTIVE;                   // Camera mode type
+
+    SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+}
+
+
 
 int main(int argc, char* argv[]) {
-    game_context = game_init_context();
-    cube_init_world(&levels[LEVEL_CUBE], &game_context, argc, argv);
-    height_map_init_world(&levels[LEVEL_HEIGHT_MAP], &game_context, argc, argv);
+    ecs_world_t *world;
 
-    game_context.world = levels[LEVEL_CUBE];
+    init_camera();
 
-    current_level = LEVEL_CUBE;
+    world = ecs_init_w_args(argc, argv);
 
+    ECS_TAG(world, Rendereable);
+    ECS_COMPONENT(world, Vector3);
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(game_update, 0, 1);
-#else
+    ECS_SYSTEM(world, cube_render, EcsOnUpdate, Vector3, Rendereable);
 
-    while(ecs_progress(game_context.world, 0) && !WindowShouldClose()){
-        handleControllers();
-    }
-    game_fini(&game_context);
-    for(int i = 0; i < LEVEL_COUNT; i++){
-        ecs_fini(levels[i]);
-    }
-#endif
+    ECS_SYSTEM(world, cube_init_position, EcsOnAdd, Vector3);
+
+    ECS_SYSTEM(world, cube_update_position, EcsOnUpdate, Vector3);
+
+    ECS_ENTITY(world, cube, Vector3, Rendereable);
+    ecs_set_target_fps(world, 60);
+
+    while(ecs_progress(world, 0) && !WindowShouldClose());
+
+    ecs_fini(world);
+    CloseWindow();
 }
