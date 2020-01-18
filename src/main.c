0da62fda8d3d7a9c5b199446e053f765f9eb0911
WEREMSOFT <pablo.weremczuk@gmail.com>
2020-01-18 16:52:25 +0100

    [ADD] Added support for raylib
         - Added raylib library and a make file to compile to hmtl

--- src/main.c
+++ src/main.c
@@ -1,14 +1,3 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Initialize 3d camera free
-*
-*   This example has been created using raylib 1.3 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
